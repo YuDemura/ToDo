0648f0f14900230fa7f339db9a0fd79688d43c3b
YuDemura <Yu.demura1214@gmail.com>
2022-05-27 21:25:48 +0900

    カテゴリ削除リファクタリング

--- src/public/category/delete.php
+++ src/public/category/delete.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-$dbUserName = 'root';
-$dbPassword = 'password';
-$pdo = new PDO('mysql:host=mysql; dbname=todo; charset=utf8', $dbUserName, $dbPassword);
+
+require_once __DIR__ . '/../utils/pdo.php';
 
 $category_id = filter_input(INPUT_GET, 'id');
 $user_id = $_SESSION['user_id'];
